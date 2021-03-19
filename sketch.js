8f57fcf2604a244f3296b5dc8882578338b3860e
potato3010 <76241784+potato3010@users.noreply.github.com>
2021-03-19 14:25:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,64 @@
-const Engine =Matter.Engine;
-const World= Matter.World;
-const Bodies= Matter.Bodies;
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
 
-var engine, world;
-var ground;
-var ball;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
 
 function setup() {
- var canvas= createCanvas(400,400);
- engine = Engine.create();
- world= engine.world;
+	createCanvas(800, 750);
 
-var ground_options ={
-  isStatic:true
+	// fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
 }
 
- ground= Bodies.rectangle(200,390,200,20,ground_options );
- World.add(world,ground);
- ball = Bodies.circle(200,100,20)
- World.add(world,ball);
-}
 
 function draw() {
-  background(0); 
-  Engine.update(engine);
-  rectMode(CENTER);
- rect(ground.position.x,ground.position.y,400,20);
- ellipseMode(RADIUS);
-
- ellipse(ball.position.x,ball.position.y,20,20);
+  background(bgImg);
+  
  
+  drawSprites();
+  keyPressed();
+}
+
+function keyPressed() {
+	//write code here
+	if(keyDown("right")){
+		fairy.x=fairy.x+3
+		}
+		if(keyDown("left")){
+		  fairy.x=fairy.x-3
+		  }
+		  if(keyDown("down")){
+			  star.y=star.y+100
+			  }
+
 
 }
